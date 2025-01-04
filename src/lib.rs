ca42a544be9992ee1a0036ce096d08cc306b4a23
Arine <peppydays@gmail.com>
2025-01-04 15:43:58 +0900

    Implemented health check test

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 use std::io::Error;
-use std::net::{Ipv4Addr, SocketAddrV4};
 
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
@@ -11,12 +10,7 @@ pub async fn check_health() -> impl IntoResponse {
     StatusCode::OK
 }
 
-pub async fn run() -> Result<(), Error> {
-    let listener = TcpListener::bind(SocketAddrV4::new(Ipv4Addr::LOCALHOST, 8080))
-        .await
-        .expect("Failed to bind a port");
-
-    let app = Router::new().route("/health_check", get(check_health));
-
+pub async fn run(listener: TcpListener) -> Result<(), Error> {
+    let app = Router::new().route("/healthz", get(check_health));
     axum::serve(listener, app).await
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 use std::io::Error;
+use std::net::Ipv4Addr;
+use std::net::SocketAddrV4;
+
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    zero2prod::run().await
+    let listener = TcpListener::bind(SocketAddrV4::new(Ipv4Addr::LOCALHOST, 8080))
+        .await
+        .expect("Failed to bind a port");
+
+    zero2prod::run(listener).await
 }
