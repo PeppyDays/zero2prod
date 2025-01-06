6dec859815fc72de2cf06c643570eb4db5657157
Arine <peppydays@gmail.com>
2025-01-07 06:41:09 +0900

    Added database connection pool in the app

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,32 @@
 use std::error::Error;
 use std::net::Ipv4Addr;
 use std::net::SocketAddrV4;
+use std::time::Duration;
 
+use sqlx::postgres::PgPoolOptions;
 use tokio::net::TcpListener;
+use zero2prod::configuration;
+use zero2prod::startup;
 
 #[tokio::main]
 async fn main() -> Result<(), impl Error> {
-    let listener = TcpListener::bind(SocketAddrV4::new(Ipv4Addr::LOCALHOST, 8080))
+    let configuration = configuration::get_configuration("configuration.yaml")
+        .expect("Failed to read configuration");
+
+    let address = SocketAddrV4::new(Ipv4Addr::LOCALHOST, configuration.application.port);
+    let listener = TcpListener::bind(address)
         .await
         .expect("Failed to bind a port");
 
-    zero2prod::startup::run(listener).await
+    let database_pool = PgPoolOptions::new()
+        .min_connections(5)
+        .max_connections(5)
+        .acquire_timeout(Duration::from_secs(5))
+        .connect(configuration.database.connection_string().as_str())
+        .await
+        .expect("Failed to create database connection pool");
+
+    let state = startup::AppState { database_pool };
+
+    startup::run(listener, state).await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,15 +1,30 @@
 use std::error::Error;
 
+use axum::extract::FromRef;
 use axum::routing::get;
 use axum::routing::post;
 use axum::Router;
+use sqlx::Pool;
+use sqlx::Postgres;
 use tokio::net::TcpListener;
 
 use crate::routes;
 
-pub async fn run(listener: TcpListener) -> Result<(), impl Error> {
+#[derive(Clone)]
+pub struct AppState {
+    pub database_pool: Pool<Postgres>,
+}
+
+impl FromRef<AppState> for Pool<Postgres> {
+    fn from_ref(state: &AppState) -> Self {
+        state.database_pool.clone()
+    }
+}
+
+pub async fn run(listener: TcpListener, state: AppState) -> Result<(), impl Error> {
     let app = Router::new()
         .route("/subscriptions", post(routes::subscriptions::subscribe))
+        .with_state(state)
         .route("/healthz", get(routes::health_check::check_health));
 
     axum::serve(listener, app).await
