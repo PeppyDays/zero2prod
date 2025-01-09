7341443d466d24bc60e94250bef17dc06324e632
Arine <peppydays@gmail.com>
2025-01-09 17:44:50 +0900

    Added tracing for new subscribers

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -16,24 +16,36 @@ pub struct Request {
     email: String,
 }
 
+#[tracing::instrument(name = "Adding a new subscriber", skip_all, fields(email = %request.email))]
 pub async fn subscribe(
     State(pool): State<Pool<Postgres>>,
     Form(request): Form<Request>,
 ) -> impl IntoResponse {
-    match sqlx::query!(
-        r#"
-            INSERT INTO subscriptions (id, name, email, subscribed_at)
-            VALUES ($1, $2, $3, $4)
-        "#,
-        Uuid::new_v7(Timestamp::now(ContextV7::new())),
-        request.name,
-        request.email,
-        Utc::now().naive_utc(),
-    )
-    .execute(&pool)
-    .await
-    {
+    match insert_subscriber(&pool, &request.name, &request.email).await {
         Ok(_) => StatusCode::OK,
         Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
     }
+}
+
+#[tracing::instrument(name = "Saving a new subscriber details in the database", skip_all)]
+async fn insert_subscriber(
+    pool: &Pool<Postgres>,
+    name: &str,
+    email: &str,
+) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        "INSERT INTO subscriptions (id, name, email, subscribed_at) VALUES ($1, $2, $3, $4)",
+        Uuid::new_v7(Timestamp::now(ContextV7::new())),
+        name,
+        email,
+        Utc::now().naive_utc(),
+    )
+    .execute(pool)
+    .await
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+
+    Ok(())
 }
